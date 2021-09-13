fd041c9628001d58f4256336f8eeed5899ddf69b
Ken Kellner <ken@kenkellner.com>
2021-09-13 15:14:27 -0400

    Add smoothing option to plot_marginal and evaluate at medians instead of means of covariates

--- R/plot_marginal.R
+++ R/plot_marginal.R
@@ -5,13 +5,18 @@ setGeneric("plot_marginal", function(object, ...) standardGeneric("plot_marginal
 #' Generates marginal fixed effects plots of one or more covariates from a
 #' \code{ubmsFit} submodel. For each plot, the focal covariate is allowed to
 #' vary across its range (or possible discrete values, for a factor), while
-#' the other covariates are held at their means or reference levels.
+#' the other covariates are held at their medians or reference levels.
 #' Random effects are ignored.
 #'
 #' @param object A fitted model of class \code{ubmsFit}
 #' @param submodel Submodel to get plots for, for example \code{"det"}
 #' @param covariate Plot a specific covariate; provide the name as a string
 #' @param level Probability mass to include in the uncertainty interval
+#' @param smooth Smoother span (f) value used for LOWESS smoothing of the
+#'  upper and lower credible interval bounds for a continuous covariate.
+#'  No smoothing is done if \code{NULL}. A reasonable value to try is 0.05.
+#'  The larger the value, the smoother the curve. As with all smoothing, use
+#'  with caution
 #' @param ... Currently ignored
 #'
 #' @return A \code{ggplot}, potentially with multiple panels, one per covariate
@@ -22,17 +27,17 @@ setGeneric("plot_marginal", function(object, ...) standardGeneric("plot_marginal
 #' @importFrom ggplot2 geom_errorbar
 #' @export
 setMethod("plot_marginal", "ubmsFit", function(object, submodel, covariate=NULL,
-                                               level=0.95, ...){
+                                               level=0.95, smooth=NULL, ...){
 
   sm <- object[submodel]
 
   if(!is.null(covariate)){
-    plots <- list(marginal_covariate_plot(object, submodel, covariate, level))
+    plots <- list(marginal_covariate_plot(object, submodel, covariate, level, smooth))
   } else {
     vars <- all.vars(remove_offset(lme4::nobars(sm@formula)))
     if(length(vars) == 0) stop("No covariates in this submodel")
     plots <- lapply(vars, function(x){
-                    marginal_covariate_plot(object, submodel, x, level)
+                    marginal_covariate_plot(object, submodel, x, level, smooth)
                     })
   }
 
@@ -47,7 +52,7 @@ setMethod("plot_marginal", "ubmsFit", function(object, submodel, covariate=NULL,
                           gp=grid::gpar(fontsize=14)))
 })
 
-marginal_covariate_plot <- function(object, submodel, covariate, level=0.95){
+marginal_covariate_plot <- function(object, submodel, covariate, level=0.95, smooth=NULL){
   sm <- object[submodel]
   quant <- c((1-level)/2, level+(1-level)/2)
   vars <- all.vars(lme4::nobars(sm@formula))
@@ -56,18 +61,24 @@ marginal_covariate_plot <- function(object, submodel, covariate, level=0.95){
   if(is_factor){
     return(marg_factor_plot(object, submodel, covariate, quant))
   }
-  marg_numeric_plot(object, submodel, covariate, quant)
+  marg_numeric_plot(object, submodel, covariate, quant, smooth)
 }
 
-marg_numeric_plot <- function(object, submodel, covariate, quant){
+marg_numeric_plot <- function(object, submodel, covariate, quant, smooth=NULL){
   sm <- object[submodel]
   samples <- get_samples(object, 1000)
-  newdata <- get_mean_df(sm)[rep(1, 1000),,drop=FALSE]
+  newdata <- get_baseline_df(sm)[rep(1, 1000),,drop=FALSE]
   var_range <- range(sm@data[[covariate]], na.rm=TRUE)
   newdata[[covariate]] <- seq(var_range[1], var_range[2], length.out=1000)
 
   plot_df <- get_margplot_data(object, submodel, covariate, quant,
                                samples, newdata)
+
+  # Smooth upper and lower CI bounds if requested
+  if(!is.null(smooth)){
+    plot_df$lower <- stats::lowess(plot_df$lower, f=smooth)[[2]]
+    plot_df$upper <- stats::lowess(plot_df$upper, f=smooth)[[2]]
+  }
 
   ggplot(data=plot_df, aes_string(x="covariate", y="mn")) +
     geom_ribbon(aes_string(ymin="lower", ymax="upper"), alpha=0.3) +
@@ -81,7 +92,7 @@ marg_factor_plot <- function(object, submodel, covariate, quant){
   sm <- object[submodel]
   samples <- get_samples(object, 1000)
   nlev <- nlevels(sm@data[[covariate]])
-  newdata <- get_mean_df(sm)[rep(1, nlev),,drop=FALSE]
+  newdata <- get_baseline_df(sm)[rep(1, nlev),,drop=FALSE]
   newdata[[covariate]] <- levels(sm@data[[covariate]])
 
   plot_df <- get_margplot_data(object, submodel, covariate, quant,
@@ -95,13 +106,13 @@ marg_factor_plot <- function(object, submodel, covariate, quant){
     theme(axis.title.y=element_blank())
 }
 
-get_mean_df <- function(submodel){
+get_baseline_df <- function(submodel){
   vars <- all.vars(lme4::nobars(submodel@formula))
   out <- lapply(vars, function(x, data){
                  if(ubms:::col_is_factor(x, data)){
                    return(factor(levels(data[[x]])[1], levels=levels(data[[x]])))
                  }
-                 return(mean(data[[x]], na.rm=TRUE))
+                 return(median(data[[x]], na.rm=TRUE))
                  }, data=submodel@data)
   out <- as.data.frame(out)
   names(out) <- vars
